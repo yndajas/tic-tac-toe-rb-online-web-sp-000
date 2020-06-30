91162a2064df78a94a13debfc65865cff87d8f50
Ynda Jas <yndajas@gmail.com>
2020-06-30 03:36:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,56 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, player = "X")
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  turn
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
 def won?(board)
   WIN_COMBINATIONS.each do |combination|
     if combination.all? {|index|board[index] == "X"}
@@ -35,42 +85,6 @@ end
 def winner(board)
   if won?(board)
     board[won?(board)[0]]
-  end
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
   end
 end
 
